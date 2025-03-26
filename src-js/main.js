ca48e6ac3dd52528ff0afa8805e653aa44f96f32
Simbarashe Madungwe <simbahm@Simbarashes-MacBook-Pro.local>
2025-03-26 20:25:56 +0200

    layout fix

--- src-js/main.js
+++ src-js/main.js
@@ -4,13 +4,3 @@ const navContainer = document.querySelector(".nav-container")
 menuIconContainer.addEventListener("click", () => {
 	navContainer.classList.toggle("active")
 })
-
-function openNav() {
-	document.getElementById("myNav").style.display = "block"
-	document.body.classList.add("hide")
-}
-
-function closeNav() {
-	document.getElementById("myNav").style.display = "none"
-	document.body.classList.remove("hide")
-}
