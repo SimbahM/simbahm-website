1e7ddea19ee13b4f3599c9507214e9ece6950c58
Simbarashe Madungwe <simbahm@Simbarashes-MacBook-Pro.local>
2025-04-05 23:24:26 +0200

    layout fix

--- src-js/main.js
+++ src-js/main.js
@@ -1,6 +1,22 @@
-const menuIconContainer = document.querySelector("nav .menu-icon-container")
-const navContainer = document.querySelector(".nav-container")
+const menuIcon = document.querySelector("nav .menu-icon")
+const mobileNavItems = document.querySelector("nav .mobile-nav-items")
+const mobileMegaMenu = document.querySelector("nav .mobile-mega-menu")
+const mobileMegaMenuController = document.querySelector(
+	"nav .mobile-nav-items ul li .mobile-mega-menu-controller"
+)
+const backIcon = document.querySelector(
+	"nav .mobile-mega-menu .items .back-icon"
+)
 
-menuIconContainer.addEventListener("click", () => {
-	navContainer.classList.toggle("active")
+menuIcon.addEventListener("click", () => {
+	mobileNavItems.classList.toggle("active")
+	menuIcon.classList.toggle("active")
+})
+
+mobileMegaMenuController.addEventListener("click", () => {
+	mobileMegaMenu.classList.add("active")
+})
+
+backIcon.addEventListener("click", () => {
+	mobileMegaMenu.classList.remove("active")
 })
